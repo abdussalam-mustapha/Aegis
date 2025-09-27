fef74bc8d677b5b5f67296cb0994f31f3f5ce8a8
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2025-09-27 02:18:02 +0100

    update scripts

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // or 'dist', but try 'build' if Vercel conflicts
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        dir: 'build',
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
-})
+    outDir: "build",       
+    emptyOutDir: true,    
+    assetsDir: "assets",   
+  },
+});
